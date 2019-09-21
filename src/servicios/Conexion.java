eaa1bcd84f49ee7f8cfba597597e691a55b9fe75
rodri <rodri@DESKTOP-QCGIQVD>
2019-09-21 12:08:05 -0300

    modularizando por paquetes

---
+++ src/servicios/Conexion.java
@@ -0,0 +1,27 @@
+package servicios;
+import java.sql.*;
+public class Conexion {
+   private static Connection cnx = null;
+   public static Connection obtener() throws SQLException, ClassNotFoundException {
+      if (cnx == null) {
+             String url = "jdbc:mysql://localhost:3306/";
+             String user = "root";
+             String pass = "";
+             System.out.println("Conectando…");
+         try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cnx = DriverManager.getConnection(url, user,pass);
+         } catch (SQLException ex) {
+            throw new SQLException(ex);
+         } catch (ClassNotFoundException ex) {
+            throw new ClassCastException(ex.getMessage());
+         }
+      }
+      return cnx;
+   }
+   public static void cerrar() throws SQLException {
+      if (cnx != null) {
+         cnx.close();
+      }
+   }
+}
